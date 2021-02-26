1bb11039dc5591d457881ed3bc3a464c2712145f
Elijah R <groovydingo@yahoo.com>
2021-02-26 17:58:42 +1030

    UID and username added to db on login change

--- public/app.js
+++ public/app.js
@@ -1,13 +1,17 @@
 const txtEmail = document.querySelector('#txtEmail');
 const txtPassword = document.querySelector('#txtPassword');
+const txtUsername = document.querySelector('#txtUsername');
 const btnSignIn = document.querySelector('#btnSignIn');
 const btnSignUp = document.querySelector('#btnSignUp');
 const btnSignOut = document.querySelector('#btnSignOut');
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+const users = db.collection('users');
 
 btnSignIn.addEventListener('click', e => {
-    const email = txtEmail.value;
-    const password = txtPassword.value;
-    const auth = firebase.auth();
+    let email = txtEmail.value;
+    let password = txtPassword.value;
 
     const promise = auth.signInWithEmailAndPassword(email, password);
     promise.catch(e => console.log(e.message));
@@ -15,9 +19,8 @@ btnSignIn.addEventListener('click', e => {
 
 btnSignUp.addEventListener('click', e => {
     // TODO: Check for real email.
-    const email = txtEmail.value;
-    const password = txtPassword.value;
-    const auth = firebase.auth();
+    let email = txtEmail.value;
+    let password = txtPassword.value;
 
     const promise = auth.createUserWithEmailAndPassword(email, password);
     promise.catch(e => console.log(e.message));
@@ -31,8 +34,26 @@ firebase.auth().onAuthStateChanged(firebaseUser => {
     if (firebaseUser) {
         console.log(firebaseUser);
         btnSignOut.classList.remove('hidden');
+        writeUserToDB();
+
     } else {
         console.log('not logged in.');
         btnSignOut.classList.add('hidden');
     }
 });
+
+function writeUserToDB() {
+    let username = txtUsername.value;
+    let uid = auth.currentUser.uid;
+
+    users.doc(uid).set({
+            username: username,
+            uid: uid
+        })
+        .then(() => {
+            console.log("Document successfully written!");
+        })
+        .catch((error) => {
+            console.error("Error writing document: ", error);
+        });
+};
