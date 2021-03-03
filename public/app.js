7f6434a18266ecfe43fe6fe0fb3b629057e315ad
Elijah R <groovydingo@yahoo.com>
2021-03-03 23:25:02 +1030

    style buttons

--- public/app.js
+++ public/app.js
@@ -1,3 +1,4 @@
+const loginPage = document.querySelector('#login-page');
 const txtEmail = document.querySelector('#txtEmail');
 const txtPassword = document.querySelector('#txtPassword');
 const txtUsername = document.querySelector('#txtUsername');
@@ -9,6 +10,7 @@ const db = firebase.firestore();
 
 const users = db.collection('users');
 
+// Sign existing user in
 btnSignIn.addEventListener('click', e => {
     let email = txtEmail.value;
     let password = txtPassword.value;
@@ -17,6 +19,7 @@ btnSignIn.addEventListener('click', e => {
     promise.catch(e => console.log(e.message));
 });
 
+// Sign up existing user
 btnSignUp.addEventListener('click', e => {
     // TODO: Check for real email.
     let email = txtEmail.value;
@@ -26,34 +29,54 @@ btnSignUp.addEventListener('click', e => {
     promise.catch(e => console.log(e.message));
 });
 
+// Sign out current user
 btnSignOut.addEventListener('click', e => {
     firebase.auth().signOut();
 });
 
+// Detecy when auth state changes
 firebase.auth().onAuthStateChanged(firebaseUser => {
+    // If signed in
     if (firebaseUser) {
         console.log(firebaseUser);
-        btnSignOut.classList.remove('hidden');
-        writeUserToDB();
+        btnSignOut.classList.remove('hidden'); // Show logout button
 
-    } else {
+        // Check user exists in db
+        users.doc(auth.currentUser.uid).get().then((doc) => {
+            if (doc.exists) { // User exists
+                console.log("Document data:", doc.data());
+            } else { // Add user data to db
+                console.log("No such document!");
+                writeUserToDB();
+            }
+
+            loginPage.classList.add('hidden');
+        }).catch((error) => { // Catch any errors
+            console.log("Error getting document:", error);
+        });
+
+    } else { // Confirm logout
         console.log('not logged in.');
         btnSignOut.classList.add('hidden');
+        loginPage.classList.remove('hidden');
     }
 });
 
+// Add new user information to database
 function writeUserToDB() {
     let username = txtUsername.value;
     let uid = auth.currentUser.uid;
+    let email = txtEmail.value;
 
-    users.doc(uid).set({
+    users.doc(uid).set({ // Write to db
             username: username,
-            uid: uid
+            uid: uid,
+            email: email
         })
-        .then(() => {
+        .then(() => { // If success
             console.log("Document successfully written!");
         })
-        .catch((error) => {
+        .catch((error) => { // Catch errors
             console.error("Error writing document: ", error);
         });
 };
